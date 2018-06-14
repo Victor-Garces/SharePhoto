6d126198b63405b86482a0c85c7766f394f54709
victorgarcesg <victorgarcesgg@gmail.com>
2018-06-14 00:28:33 -0400

    Added the add picture button
    A button in the top bar to open the feature of take a picture from the camera

--- app/src/main/java/com/example/victor/sharephoto/LoginSuccessActivity.java
+++ app/src/main/java/com/example/victor/sharephoto/LoginSuccessActivity.java
@@ -1,7 +1,12 @@
 package com.example.victor.sharephoto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class LoginSuccessActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class LoginSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_picture:
+                Toast.makeText(this, "Added add button", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
